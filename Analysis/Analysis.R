7dc6b011a27deb4e6115f6d2a337ccc27c493880
oksanabuzh <oksana.buzh@fu-berlin.de>
2023-02-22 18:05:20 +0100

    analysis

--- Analysis/Analysis.R
+++ Analysis/Analysis.R
@@ -1,7 +1,7 @@
 # Code for summary statistics and visualization of results
-# for Buzhdygan & Petermann
+# Buzhdygan & Petermann
 # "Multitrophic aspects of ecological intensification and multiple ecosystem functions and services"
-# in Journal of Plant Ecology
+# Journal of Plant Ecology
 
 # Required packages
 
